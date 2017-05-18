adeaa9afd5d1f39b4bdce4494ca98448623d4712
Sam <mcmyler@gmail.com>
2017-05-18 15:15:42 -0500

    Refactor for fcc

--- js/controller.js
+++ js/controller.js
@@ -8,93 +8,43 @@ var controller = function () {
   var calcModel = model();
 
   var numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero', 'decimal', 'neg'];
-  var operators = ['add', 'subtract', 'multiple', 'divide'];
+  var operators = ['add', 'subtract', 'multiply', 'divide'];
 
   function keyClicked(key) {
     if (numbers.includes(key)) {
-      calcModel.updateNumString(key);
+      calcModel.updateNumString(key)
     } else if (operators.includes(key)) {
-      if (calcModel.lastKeyClicked === 'equals') {
-        var totalStr = calcModel.getTotal().toString()
-        calcModel.updateNumString(totalStr);
-      }
-
-      if (calcModel.operatorCount() > 0) {
-        calcModel.output.push(calcModel.getNumString());
-        calcModel.resetNumString();
-        var result = getResult(calcModel.output);
-        calcModel.setTotal(+result);
-        calcModel.resetOperatorList();
-        calcModel.output = [];
-        calcModel.output.push(result.toString());
-        calcModel.insertOperator(key);
-      } else {
-        calcModel.output.push(calcModel.getNumString());
-        calcModel.resetNumString();
-        calcModel.insertOperator(key);
-      }
-    } else if (key === '%') {
-      if (calcModel.lastOperator() !== '') {
-        var last = calcModel.output.length - 1;
-        var pNum = calcModel.output[last];
-        var cNum = +calcModel.getNumString();
-        var num = cNum / 100 * pNum;
-        calcModel.output.push(num.toString());
-        calcModel.resetNumString();
-        calcModel.setTotal(num);
-      } else {
-        calcModel.output.push(calcModel.getNumString());
-        calcModel.resetNumString();
-        var last = calcModel.output.length - 1;
-        var result = calculator.percent(calcModel.output[last]);
-        calcModel.setTotal(+result);
-        calcModel.output[last] = result.toString();
-      }
-    } else if (key === 'clear') {
-      if (calcModel.getAllClear()) {
-        calcModel.setTotal(0);
-        calcModel.resetNumString();
-        calcModel.resetOperatorList();
-        calcModel.output = [];
-      } else {
-        calcModel.resetNumString();
-        calcModel.setAllClear();
-      }
+      calcModel.insertOperator(key)
+      calcModel.setTotal(calcModel.getNumString())
+      calcModel.resetNumString()
     } else if (key === 'equals') {
-      calcModel.output.push(calcModel.getNumString());
-      calcModel.resetNumString();
-      var result = getResult(calcModel.output);
-      calcModel.setTotal(+result);
-      calcModel.resetOperatorList();
-      calcModel.output = [];
+      var total = calculate()
+      calcModel.setTotal(total)
+      calcModel.resetNumString()
     }
-    calcModel.lastKeyClicked = key;
   }
 
-  function getResult(arr) {
-    return arr.filter(function (str) {
-      return str !== '';
-    }).reduce(function (pVal, cVal) {
-      return calculate(+pVal, +cVal);
-    });
-  }
+  function calculate() {
+    var operator = calcModel.lastOperator()
+    var a = calcModel.getTotal()
+    var b = +calcModel.getNumString()
 
-  function calculate(x, y) {
-    var operator = calcModel.lastOperator();
-    var operatorKeyToFunction = {
-      'add': calculator.add(x, y),
-      'subtract': calculator.subtract(x, y),
-      'multiply': calculator.multiply(x, y),
-      'divide': calculator.divide(x, y),
-      '%': calculator.percent(x),
+    switch (operator) {
+      case 'add':
+        return calculator.add(a, b)
+      case 'subtract':
+        return calculator.subtract(a, b)
+      case 'divide':
+        return calculator.divide(a, b)
+      case 'multiply':
+        return calculator.multiply(a, b)
     }
-    return operatorKeyToFunction[operator];
   }
 
   return {
     keyClicked: keyClicked,
     calcModel: calcModel,
-    calculate: calculate,
+    calculate: calculate
   }
 }
 
